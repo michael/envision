87b14408470c3f9cc3043814fb162aac92a7d13f
michael <ma@zive.at>
2010-05-07 00:34:46 +0200

    cleanup

--- app/controllers/browsing_sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class BrowsingSessionsController < ApplicationController
-  
-  # /collections/:collection_id/browse
-  def browse
-    @collection = Collection.get(params[:collection_id])
-    # should we store the current BrowsingContext based in the session?
-    @browsing_session = BrowsingSession.new(@collection, FilterCriteria.new(params[:criteria] || {}))
-  end
-end
