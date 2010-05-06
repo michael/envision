11218d9325c0e3a6be4d4158527d4c05e2020c88
michael <ma@zive.at>
2010-05-07 00:35:08 +0200

    filter criteria per session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  # TODO: re-enable forgery protection
+  # protect_from_forgery
+  
   layout 'application'
+  
+  def update_filter_criteria(filter_criteria)
+    session[:filter_criteria] = filter_criteria
+    @filter_criteria = FilterCriteria.new(session[:filter_criteria])
+  end
+  
+  def filter_criteria
+    @filter_criteria ||= FilterCriteria.new(session[:filter_criteria] || {})
+  end
 end
