21c0ecd0066d7a76e5b6c69bb6800bf7ee5b4f0c
michael <ma@zive.at>
2010-05-13 03:25:03 +0200

    Use the brand-new APIs.

--- public/javascripts/browse.js
+++ public/javascripts/browse.js
@@ -8,7 +8,7 @@ var Templates = {
   facets: ' \
   <a href="#" class="clear-filters">Clear filters</a><br/><br/> \
     {{#facets}} \
-      <h3>{{property}}</h3> \
+      <h3>{{property_name}}</h3> \
       <ul> \
         {{#facet_choices}} \
           <li><a class="facet-choice" href="{{property}}">{{value}}</a> ({{item_count}})</li> \
@@ -22,15 +22,27 @@ var Templates = {
   view: ' \
     <canvas id="chart"></canvas> \
     <div id="view-settings"> \
-      measures: \
-      <select id="measures" multiple="multiple" name="measures[]"> \
+      <h4>measures</h4> \
+      <select id="measure_keys" multiple="multiple" name="measure_keys[]" style="width: 180px; height: 100px"> \
         {{#properties}} \
-          <option {{#selected}}selected="selected"{{/selected}} value="{{key}}">{{name}}</option> \
+          <option {{#measureKeySelected}}selected="selected"{{/measureKeySelected}} value="{{key}}">{{name}} ({{type}})</option> \
         {{/properties}} \
       </select> \
+      <h4>identifiedBy</h4> \
+      <select id="identity_keys" multiple="multiple" name="identity_keys[]" style="width: 180px; height: 100px"> \
+        {{#properties}} \
+          <option {{#identityKeySelected}}selected="selected"{{/identityKeySelected}} value="{{key}}">{{name}} ({{type}})</option> \
+        {{/properties}} \
+      </select> \
+      <h4>groupBy</h4> \
+      <select id="group_keys" multiple="multiple" name="group_keys[]" style="width: 180px; height: 100px"> \
+        {{#properties}} \
+          <option {{#groupKeySelected}}selected="selected"{{/groupKeySelected}} value="{{key}}">{{name}} ({{type}})</option> \
+        {{/properties}} \
+      </select> \
+      Aggregieren <input id="aggregate" name="aggregate" type=checkbox value="1"/> \
     </div>'
-}
-
+};
 
 //-----------------------------------------------------------------------------
 // FilterCriteria
@@ -38,7 +50,7 @@ var Templates = {
 
 var FilterCriteria = function() {
   this.filterCriteria = {};
-}
+};
 
 FilterCriteria.prototype = {
   addCriterion: function(key, value) {
@@ -53,7 +65,7 @@ FilterCriteria.prototype = {
   getCriteria: function() {
     return this.filterCriteria;
   }
-}
+};
 
 var filters = new FilterCriteria();
 
@@ -66,31 +78,45 @@ var filters = new FilterCriteria();
 
 var View = function(collection, options) {
   this.collection = collection;
-  this.measures = [];
+  
+  this.measureKeys = [];
+  this.identityKeys = options.identity_keys || [];
+  this.groupKeys = options.group_keys || [];
+  this.aggregate = false;
+  
   this.id = options.id;
   this.collectionId = options.collectionId;
   var that = this;
   
   // init measures
   $.each(options.measures, function(index, property) {
-    that.measures.push(property);
+    that.measureKeys.push(property);
   });
+  
+  
 }
 
 View.prototype = {
   update: function() {
-    this.measures = $('select#measures').val() || [];
+    this.measureKeys = $('select#measure_keys').val() || [];
+    this.identityKeys = $('select#identity_keys').val() || [];
+    this.groupKeys = $('select#group_keys').val() || [];
+    this.aggregate = $('input#aggregate').is(':checked');
+    
     var that = this;
-    
     this.renderChart();
-    
+
     // store view changes on the server
     $.ajax({
       url: '/collections/'+this.collectionId+'/views/'+this.id+'.json',
       type: 'put',
-      data: { measures: that.measures },
+      data: {
+        measure_keys: that.measureKeys,
+        identity_keys: that.identityKeys,
+        group_keys: that.groupKeys
+      },
       success: function(json) {
-        console.log("measures updated");
+        console.log("view settings updated");
       }
     });
   },
@@ -102,34 +128,48 @@ View.prototype = {
         var result = [];
         
         $.each(that.collection.properties, function(key, p) {
-          if (p.type === "number") {
-            
-            result.push({key: key, name: p.name, selected: $.inArray(key, that.measures) > -1});
-          }
+          result.push({
+            key: key,
+            name: p.name,
+            type: p.type,
+            measureKeySelected: $.inArray(key, that.measureKeys) > -1,
+            identityKeySelected: $.inArray(key, that.identityKeys) > -1,
+            groupKeySelected: $.inArray(key, that.groupKeys) > -1
+          });
         });
         return result;
       }
     };
 
+    // render results template
     $('#results').html($.mustache($.template('view'), v));
-    $('select#measures').change(function() {
+    
+    $('#view-settings select').change(function() {
       that.update();
     });
-    
+    $('#view-settings input').change(function() {
+      that.update();
+    });
+
     that.renderChart();
   },
   renderChart: function() {
     // quick and dirty scatter plot
-    // let the chart decide if it's able to render measures.length measures
-    if (this.measures.length >= 2) {
+    // TODO: let the chart decide if it's able to render measures.length measures
+    if (this.measureKeys.length >= 2) {
       $('#chart').chart('destroy');
+      
+      // prepare grouping options
+      var groupKeys = $.map(this.groupKeys, function(k) { return {property: k, modifier: Modifiers.DEFAULT}; });
+      
       $('#chart').chart({
           collection: this.collection,
           plotOptions: {
             plotType: 'scatter',
-            categorizeBy: 'gender',
-            identifyBy: 'name',
-            measures: this.measures
+            groupBy: groupKeys, // list of groupkeys
+            aggregate: this.aggregate,
+            identifyBy: this.identityKeys, // salesman, customer name
+            measures: this.measureKeys
           }
       });
     }
@@ -173,7 +213,7 @@ var Client = {
         facets: function() {
           result = []
           $.each(facets, function(key, choices) {
-            result.push({property: key, facet_choices: function() {
+            result.push({property: key, property_name: collection.properties[key].name, facet_choices: function() {
               return $.map(choices, function(fc) { return {value: fc.value, item_count: fc.item_count} });
             }});
           });
@@ -204,7 +244,6 @@ var Client = {
     
     // views
     $('a.view').click(function() {
-      
       $.ajax({
         url: $(this).attr('href'),
         dataType: 'json',
@@ -213,7 +252,6 @@ var Client = {
           view.render();
         }
       });
-      
       return false;
     });
     
