beb2c4092afe3417c8ebfa0c0ebbf2c903f0db35
michael <ma@zive.at>
2010-06-06 21:55:42 +0200

    I'm going to use Sammy.

---
+++ public/javascripts/src/envision.js
@@ -0,0 +1,9 @@
+// var envision = $.sammy(function() {
+//   this.get('#/', function() {
+//     $('#wrapper').text('Nice stuff');
+//   });
+// });
+// 
+// $(function() {
+//   envision.run();
+// });
